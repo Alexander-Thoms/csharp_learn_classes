f1c49e6aa1c18ddfd941f3df265c4fc438ceb618
Alexander-Thoms <alex.g.thoms@gmail.com>
2026-09-23 10:18:23 -0400

    refactor: use underscore prefix convention for fields

--- Course.cs
+++ Course.cs
@@ -1,34 +1,39 @@
 namespace LearnClasses;
+
 class Course
 {
-    
-    private string season = "";
-    private string courseCode = "";
-    private string programName = "";
-    private string instructors = "";
+    //Fields
+    private string _season = "";
+    private string _courseCode = "";
+    private string _programName = "";
+    private string _instructors = "";
 
-    public  Course(string seasonParam, string courseCodeParam, string programNameParan, string instructorsParan)
+    // Constructors
+    public Course(string seasonParam, string courseCodeParam, string programNameParan, string instructorsParan)
     {
-        season = seasonParam;
-        courseCode = courseCodeParam;
-        programName = programNameParan;
-        instructors = instructorsParan;
+        _season = seasonParam;
+        _courseCode = courseCodeParam;
+        _programName = programNameParan;
+        _instructors = instructorsParan;
     }
+
+    // Properties
     public string GetFall
     {
-        get { return season; }
+        get { return _season; }
+        set { _season = value; }
     }
     public string getProgramName
     {
-        get{ return programName;}
+        get { return _programName; }
     }
     public string getInstructor
     {
-        get{return instructors;}
+        get { return _instructors; }
     }
     public string getCourseCode
     {
-        get{return courseCode;}
+        get { return _courseCode; }
     }
 
 }
